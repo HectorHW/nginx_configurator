71a12501e00646de480bda052555f9073a29c5d2
Vladimir Redkin <vsredkin01@gmail.com>
2023-08-08 12:41:09 +0300

    clear output directory before creating files

--- nginx_configurator/cli.py
+++ nginx_configurator/cli.py
@@ -2,8 +2,15 @@ import typer
 from nginx_configurator.config_types import parse_yaml
 from nginx_configurator.render import render_configurations
 import os
+import glob
 
 app = typer.Typer()
+
+
+def clear_output_directory(path: str):
+    files = glob.glob(os.path.join(path, "*"))
+    for f in files:
+        os.remove(f)
 
 
 @app.callback(invoke_without_command=True)
@@ -16,6 +23,8 @@ def generate_config_file(
 
     if not os.path.exists(output_directory):
         os.mkdir(output_directory)
+
+    clear_output_directory(output_directory)
 
     for filename, content in render_configurations(config_content).items():
         result_path = os.path.join(output_directory, f"{filename}.vhost")
